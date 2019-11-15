a4b9edf51115d141ca8feef55bb1368400b8485c
Edward Reyes <32741636+EdwardAsher@users.noreply.github.com>
2019-11-15 16:39:39 -0600

    Grid Update
    Grid update (in progress)

---
+++ Assets/Scripts/GridMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    List<Tile> selectibleTiles = new List<Tile>();
+    GameObject[] tiles;
+
+    Stack<Tile> path = new Stack<Tile>();
+    Tile currentTile;
+
+    public int move = 5;
+    public float jumpHeight = 2;
+    public float moveSpeed = 2;
+
+    Vector3 velocity = new Vector3();
+    Vector3 heading = new Vector3();
+
+    float halfHeight = 0;
+
+    protected void Init()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        halfHeight = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    public void GetCurrentTile()
+    {
+        currentTile = GetTargetTile(gameObject);
+        currentTile.currentTile = true;
+    }
+
+    public Tile GetTargetTile(GameObject target)
+    {
+        RaycastHit hit;
+        Tile tile = null;
+
+        if (Physics.Raycast(target.transform.position, -Vector3.up, out hit, 1))
+        {
+            tile = hit.collider.GetComponent<Tile>();
+        }
+
+        return tile;
+    }
+
+    public void ComputeAdjacencyLists()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (GameObject tile in tiles)
+        {
+            Tile t = tile.GetComponent<Tile>();
+            t.FindNeighbors(jumpHeight);
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool walkable = true;
+    public bool currentTile = false;
+    public bool target = false;
+    public bool selectable = false;
+
+    public List<Tile> adjacencyList = new List<Tile>();
+
+    public bool visited = false;
+    public Tile parent = null;
+    public int distance = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTile)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (target)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (selectable)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    public void Reset()
+    {
+        walkable = true;
+        currentTile = false;
+        target = false;
+        selectable = false;
+
+        visited = false;
+        parent = null;
+        distance = 0;
+    }
+
+    public void FindNeighbors(float jumpHeight)
+    {
+        Reset();
+
+        CheckTile(Vector3.forward, jumpHeight);
+        CheckTile(-Vector3.forward, jumpHeight);
+        CheckTile(Vector3.right, jumpHeight);
+        CheckTile(-Vector3.right, jumpHeight);
+    }
+
+    public void CheckTile(Vector3 direction, float jumpHeight)
+    {
+        Vector3 halfExtents = new Vector3(0.25f, (0.25f+jumpHeight/2.0f), 0.25f);
+        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtents);
+
+        foreach (Collider item in colliders)
+        {
+            Tile tile = item.GetComponent<Tile>();
+            if (tile != null && tile.walkable)
+            {
+                RaycastHit hit;
+
+                //Check if something above
+                if (!Physics.Raycast(tile.transform.position, Vector3.up, out hit, 1))
+                {
+                    adjacencyList.Add(tile);
+                } 
+            }
+        }
+
+    }
+}
